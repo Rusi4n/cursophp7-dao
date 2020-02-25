bf4ba804ddf4aee58b9b5e623661a5441f12b176
gercino neto <rusi4n.hys.team@gmail.com>
2020-02-24 22:03:27 -0300

    inserir e alterar

--- index.php
+++ index.php
@@ -19,9 +19,20 @@ require_once("config.php");
 // echo json_encode($search);
 
 //carrega um usuário usando o login e a senha
+// $usuario = new Usuario();
+// $usuario->login("root","123456");
+// echo $usuario;
+
+//Criando um novo usuário
+// $aluno = new Usuario("aluno", "@lun0");
+// $aluno->insert();
+// echo $aluno;
+
 $usuario = new Usuario();
-$usuario->login("root","123456");
+
+$usuario->loadById(8);
+
+$usuario->update("professor", "m3x1c0");
 
 echo $usuario;
-
  ?>
